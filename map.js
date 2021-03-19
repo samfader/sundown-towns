4b88bf89d8d78d96475080b3bf1a39e8bc45af0e
Josh Erb <josh.erb@mapbox.com>
2021-03-19 10:24:38 -0400

    little bit of clean up

--- map.js
+++ map.js
@@ -1,8 +1,9 @@
 mapboxgl.accessToken =
-  "pk.eyJ1Ijoic2FtZiIsImEiOiJjaWZ3bGhtdjgzMnN1dWdrcnEwZTVieG91In0.DkCY-91coDahKvpH7Z26dw";
-var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/samf/ckfguixbm2aee19ppd5s2d36l?fresh=true",
+  'pk.eyJ1Ijoic2FtZiIsImEiOiJjaWZ3bGhtdjgzMnN1dWdrcnEwZTVieG91In0.DkCY-91coDahKvpH7Z26dw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/samf/ckfguixbm2aee19ppd5s2d36l',
   center: [-96.829, 38.826],
   zoom: 4.3,
   pitchWithRotate: false,
@@ -14,90 +15,90 @@ var map = new mapboxgl.Map({
 });
 
 let dataUrl =
-  "https://json-loewen-sundown-towns.pantheonsite.io/sundown/database/geojson.php?";
+  'https://json-loewen-sundown-towns.pantheonsite.io/sundown/database/geojson.php';
 
 map.addControl(
   new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
-    countries: "us",
-    mapboxgl: mapboxgl,
+    countries: 'us',
+    mapboxgl,
   })
 );
 
 var nav = new mapboxgl.NavigationControl();
-map.addControl(nav, "top-left");
+map.addControl(nav, 'top-left');
 
-map.on("load", function () {
-  var legendEl = document.getElementById("legend");
-  legendEl.addEventListener("click", function (e) {
+map.on('load', function () {
+  var legendEl = document.getElementById('legend');
+  legendEl.addEventListener('click', function (e) {
     var value = e.target.innerHTML;
     filterMarkers(value);
   });
 
-  map.addSource("towns-data", {
-    type: "geojson",
+  map.addSource('towns-data', {
+    type: 'geojson',
     data: dataUrl,
   });
 
   map.addLayer(
     {
-      id: "towns",
-      type: "circle",
-      source: "towns-data",
+      id: 'towns',
+      type: 'circle',
+      source: 'towns-data',
       // filter: ['has', 'confirmed'],
       paint: {
-        "circle-color": [
-          "case",
-          ["has", "confirmed"],
+        'circle-color': [
+          'case',
+          ['has', 'confirmed'],
           [
-            "step",
-            ["get", "confirmed"],
-            "hsl(0, 0%, 82%)",
+            'step',
+            ['get', 'confirmed'],
+            'hsl(0, 0%, 82%)',
             1,
-            "#545454",
+            '#545454',
             2,
-            "#ffc6c6",
+            '#ffc6c6',
             3,
-            "#ff5e5e",
+            '#ff5e5e',
             4,
-            "#c20d0d",
+            '#c20d0d',
             8,
-            "#545454",
+            '#545454',
             9,
-            "#000000",
+            '#000000',
           ],
-          "#990000",
+          '#990000',
         ],
-        "circle-radius": 6,
-        "circle-stroke-width": 1,
-        "circle-stroke-color": [
-          "case",
-          ["has", "confirmed"],
+        'circle-radius': 6,
+        'circle-stroke-width': 1,
+        'circle-stroke-color': [
+          'case',
+          ['has', 'confirmed'],
           [
-            "step",
-            ["get", "confirmed"],
-            "hsl(0, 0%, 47%)",
+            'step',
+            ['get', 'confirmed'],
+            'hsl(0, 0%, 47%)',
             1,
-            "hsl(0, 28%, 10%)",
+            'hsl(0, 28%, 10%)',
             2,
-            "hsl(0, 100%, 62%)",
+            'hsl(0, 100%, 62%)',
             3,
-            "hsl(0, 100%, 44%)",
+            'hsl(0, 100%, 44%)',
             4,
-            "hsl(0, 88%, 20%)",
+            'hsl(0, 88%, 20%)',
             8,
-            "hsl(0, 0%, 9%)",
+            'hsl(0, 0%, 9%)',
             9,
-            "#000000",
+            '#000000',
           ],
-          ["!=", ["get", "confirmed"], null],
-          "hsl(160, 100%, 65%)",
-          "#990000",
+          ['!=', ['get', 'confirmed'], null],
+          'hsl(160, 100%, 65%)',
+          '#990000',
         ],
-        "circle-opacity": 0.75,
+        'circle-opacity': 0.75,
       },
     },
-    "settlement-subdivision-label"
+    'settlement-subdivision-label'
   );
 
   var popup = new mapboxgl.Popup({
@@ -106,15 +107,13 @@ map.on("load", function () {
   });
 
   var confirmedDescription = {
-    0: "Don't know",
-    1: "Unlikely",
-    2: "Possible",
-    3: "Probable",
-    4: "Surely",
-    8: "Always biracial",
-    9: "Black Town or Township",
-    undefined: "Don't know",
-    null: "Don't know",
+    0: 'Don\'t know',
+    1: 'Unlikely',
+    2: 'Possible',
+    3: 'Probable',
+    4: 'Surely',
+    8: 'Unlikely/Always Biracial',
+    9: 'Black Town or Township'
   };
 
   // map.on('mousemove', 'towns', function(e) {
@@ -130,77 +129,48 @@ map.on("load", function () {
 
   // display only features with the 'name' property 'USA'
 
-  map.on("mouseenter", "towns", function (e) {
-    map.getCanvas().style.cursor = "pointer";
+  map.on('mouseenter', 'towns', function (e) {
+    map.getCanvas().style.cursor = 'pointer';
     var coordinates = e.features[0].geometry.coordinates.slice();
     var features = map.queryRenderedFeatures(e.point);
     var id = features[0].id;
     var name = features[0].properties.name;
     var state = features[0].properties.state;
-    var confirmed = features[0].properties.confirmed;
-    var ordinance = features[0].properties.ordinance;
-    var sign = features[0].properties.sign;
-
-    console.log("confirmed is ", confirmed);
+    var confirmed = features[0].properties.confirmed || 10;
 
     popup
       .setLngLat(coordinates)
       .setHTML(
         name +
-          ", " +
+          ', ' +
           state +
-          "<br>" +
+          '<br>' +
           `Confirmed: ${confirmedDescription[confirmed]}` +
-          "<br><br>Click for more information"
+          '<br><br>Click for more information'
       )
       .addTo(map);
-    // window.open(`https://sundown.tougaloo.edu/sundowntownsshow.php?id=${id}`);
   });
 
-  map.on("mouseleave", "towns", function () {
-    map.getCanvas().style.cursor = "";
+  map.on('mouseleave', 'towns', function () {
+    map.getCanvas().style.cursor = '';
     popup.remove();
   });
 
-  map.on("click", "towns", function (e) {
+  map.on('click', 'towns', function (e) {
     var features = map.queryRenderedFeatures(e.point);
     var id = features[0].id;
     window.open(`https://sundown.tougaloo.edu/sundowntownsshow.php?id=${id}`);
   });
 
   function filterMarkers(status) {
-    console.log("status is ", status);
-    var statusInt;
-
-    switch (status) {
-      case "Don't know":
-        statusInt = 0;
-        break;
-      case "Unlikely/Always Biracial":
-        statusInt = 1;
-        break;
-      case "Possible":
-        statusInt = 2;
-        break;
-      case "Probable":
-        statusInt = 3;
-        break;
-      case "Surely":
-        statusInt = 4;
-        break;
-      case "Black Town":
-        statusInt = 9;
-        break;
-      case "Clear filter":
-        statusInt = 10;
-        break;
+    // if clear is clicked, remove filter, otherwise filter by status
+    if (status === 'Clear filter') {
+      return map.setFilter('towns', null); 
     }
 
-    // if clear is cliked, clear, otherwise filter
-    if (statusInt == 10) {
-      map.setFilter("towns", null);
-    } else {
-      map.setFilter("towns", ["==", ["get", "confirmed"], statusInt]);
+    var statusInt = Object.keys(confirmedDescription).find((key) => confirmedDescription[key] === status);
+    if (statusInt >= 0 && statusInt < 10) {
+      map.setFilter('towns', ['==', ['get', 'confirmed'], parseInt(statusInt)]);
     }
   }
 });
